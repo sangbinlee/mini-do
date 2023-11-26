33ed8c63c5a0bf7514a48deb69d35ebfbc43d7a8
sangbinlee <sangbinlee9@gmail.com>
2023-11-27 00:28:02 +0900

    NEXTAUTH_URL

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -1,5 +1,6 @@
 import { environmentVariables } from "@/schema/env";
 
 console.log('xxxxxxxxxxxxxxxx environmentVariables=', environmentVariables)
+console.log('999999999999999=', 0)
  
 export const envVariables = environmentVariables.parse(process.env)
