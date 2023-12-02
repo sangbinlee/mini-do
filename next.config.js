1fbf94a87af03a3034407e10feb331a86cdbc5b6
sangbinlee <sangbinlee9@gmail.com>
2023-12-03 05:45:27 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 const { PrismaPlugin } = require('@prisma/nextjs-monorepo-workaround-plugin')
 
 module.exports = {
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.plugins = [...config.plugins, new PrismaPlugin()]
-    }
+    webpack: (config, { isServer }) => {
+        if (isServer) {
+            config.plugins = [...config.plugins, new PrismaPlugin()]
+        }
 
-    return config
-  },
+        return config
+    },
 }
