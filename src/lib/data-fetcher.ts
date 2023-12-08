cebe97a147ebd898c7a009409ae638461f822592
sangbinlee <sangbinlee9@gmail.com>
2023-12-08 21:23:19 +0900

    tttt

--- src/lib/data-fetcher.ts
+++ src/lib/data-fetcher.ts
@@ -3,4 +3,9 @@ import axios from "axios";
 // const url: string =process.env.NODE_ENV ==="production"? "https://todo.ihemon.me":"http://localhost:3000"
 const url: string =process.env.NODE_ENV ==="production"? "https://sodi9.store":"http://localhost:3000"
 // const url: string =process.env.NODE_ENV ==="production"? "http://localhost:3000":"http://localhost:3000"
+
+
+
+console.log('★★★★★★★★★★★★★★★★★★ url=',url);
+console.log('★★★★★★★★★★★★★★★★★★ process.env.NODE_ENV=',process.env.NODE_ENV);
 export const myAxios = axios.create({baseURL:`${url}/api/`})
--- src/lib/env.ts
+++ src/lib/env.ts
@@ -1,16 +1,7 @@
 import { environmentVariables } from "@/schema/env";
-
-console.log('xxxxxxxxxxxxxxxx environmentVariables=', environmentVariables)
-console.log('999999999999999=', 0)
- 
-
-
-console.log('■■■■■■      process.env.REACT_APP_ENV=', process.env.REACT_APP_ENV);
-
-
-
-
-
-
-
 export const envVariables = environmentVariables.parse(process.env)
+
+// console.log('xxxxxxxxxxxxxxxx environmentVariables=', environmentVariables)
+console.log('999999999999999=', 0)
+console.log('■■■■■■      process.env.REACT_APP_ENV=', process.env.REACT_APP_ENV);
+console.log('■■■■■■      envVariables=', envVariables);
