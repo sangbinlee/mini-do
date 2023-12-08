6d15bbb96c7d1b66c6f9bff758ed5a0c1c6b2a3e
sangbinlee <sangbinlee9@gmail.com>
2023-12-09 03:11:06 +0900

    test

--- src/app/api/auth/[...nextauth]/authOptions.ts
+++ src/app/api/auth/[...nextauth]/authOptions.ts
@@ -4,6 +4,11 @@ import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import type { NextAuthOptions } from "next-auth";
 import GitHubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
+import KakaoProvider from "next-auth/providers/kakao";
+import NaverProvider from "next-auth/providers/naver";
+
+
+
 
 export const authOptions:NextAuthOptions = {
     adapter: PrismaAdapter(prisma),
@@ -11,11 +16,22 @@ export const authOptions:NextAuthOptions = {
       GitHubProvider({
         clientId:envVariables.GITHUB_CLIENT_ID,
         clientSecret:envVariables.GITHUB_CLIENT_SECRET,   
+        allowDangerousEmailAccountLinking: true,
       }),
-      // google OAuth
       GoogleProvider({
         clientId: process.env.GOOGLE_CLIENT_ID as string,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+        allowDangerousEmailAccountLinking: false,
+      }),
+      KakaoProvider({
+        clientId: process.env.KAKAO_CLIENT_ID as string,
+        clientSecret: process.env.KAKAO_CLIENT_SECRET as string,
+        allowDangerousEmailAccountLinking: false,
+      }),
+      NaverProvider({
+        clientId: process.env.NAVER_CLIENT_ID!,
+        clientSecret: process.env.NAVER_CLIENT_SECRET!,
+        allowDangerousEmailAccountLinking: true,
       }),
       // 일반 회원가입/로그인
       // CredentialsProvider({
@@ -33,8 +49,48 @@ export const authOptions:NextAuthOptions = {
     //   signIn: "/",
     // },
     // debug: process.env.NODE_ENV === "development",
-    session:{
-      strategy:"jwt"
-    },
+
+
+
+
+
+    // session:{
+    //   strategy:"jwt",
+    //   maxAge: 30 * 24 * 60 * 60, //30일
+    // },
     // secret: process.env.NEXTAUTH_SECRET,
+
+
+
+
+
+
+
+      
+    callbacks: {
+      //4. jwt 만들 때 실행되는 코드
+      //user변수는 DB의 유저정보담겨있고 token.user에 뭐 저장하면 jwt에 들어갑니다.
+      jwt: async ({ token, user }: any) => {
+        if (user) {
+          token.user = {}
+          token.user.name = user.name
+          token.user.email = user.email
+          token.user.image = user.image
+        }
+        console.log('★★★★★★★★★★★★★★★★★★★★★token', token);
+        return token
+      },
+      //5. 유저 세션이 조회될 때 마다 실행되는 코드
+      async session({ session, token }: any) {
+        // Send properties to the client, like an access_token and user id from a provider.
+        session.user = token.user
+        session.providerType = token.providerType
+        return session
+      },
+    },
+    // pages: {
+    //   signIn: "/",
+    // },
+    debug: process.env.NODE_ENV === "development",
+    secret: process.env.NEXTAUTH_SECRET,
   };
--- src/schema/env.ts
+++ src/schema/env.ts
@@ -2,14 +2,23 @@ import { z } from "zod";
 
 export const environmentVariables = z.object({
     DATABASE_URL: z.string(),
+
+    DIRECT_URL: z.string(),
+
+    REACT_APP_ENV: z.string(),
+
+
+
     GITHUB_CLIENT_ID: z.string(),
     GITHUB_CLIENT_SECRET: z.string(),
-    
+
     GOOGLE_CLIENT_ID: z.string(),
     GOOGLE_CLIENT_SECRET: z.string(),
 
     NEXTAUTH_SECRET: z.string(),
     NEXTAUTH_URL: z.string(),
-    DIRECT_URL:z.string()
+
+    ENV_VARIABLE:z.string(),
+    NEXT_PUBLIC_ENV_VARIABLE:z.string()
 
 })
